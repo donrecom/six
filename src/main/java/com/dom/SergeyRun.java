42f447709e5a719325cadadf1e684e03151171ea
Sergey <donrecom@gmail.comm>
2020-11-19 22:53:55 +0200

    Delete unused

--- src/main/java/com/dom/SergeyRun.java
+++ src/main/java/com/dom/SergeyRun.java
@@ -2,9 +2,12 @@ package com.dom;
 
 public class SergeyRun {
     public static void main(String[] args) {
+        SergeyRun sr = new SergeyRun();
+        System.out.println(sr.add(3, 5));
 
     }
-    public int add (int a, int b){
+
+    public int add(int a, int b) {
         return a + b;
     }
 }
