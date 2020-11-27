28cf2b0b02cbebe876fd7dc1be87d28787a1c53e
Sergey <donrecom@gmail.comm>
2020-11-27 18:01:41 +0200

    раскидываю все по классам и методам. не работает continueSR :( и че-т не хочет ставить public для кучи классов :(

--- src/main/java/com/dom/SergeyRun.java
+++ src/main/java/com/dom/SergeyRun.java
@@ -1,93 +1,151 @@
 package com.dom;
 
-// import java.lang.reflect.Array;
- import java.util.Scanner;
- import java.util.Arrays;
-// import java.util.Formatter;
+import java.util.Scanner;
+import java.util.Arrays;
 
-public class SergeyRun {  // 1.    -------------------- ВЫБОР НУЖНОЙ ПРОЦЕДУРЫ ИЗ 3Х -----------------------
+public class SergeyRun { // 1.
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Выберите номер процедуры: 1.Сумма чисел, 2.Выбор наименьшего, 3.Проверка массива слова на палиндромность, другое ==> выход.");
-        Scanner Pr=new Scanner(System.in); // ввод с клавы
-        Short pr = Pr.nextShort();              // берем переменную
-        SergeyRun sr = new SergeyRun();
-        switch (pr){
-            case 1: System.out.println("Сумма: "+sr.add());  break; //Сумма
-            case 2: sr.read_(); break; // наименьшее
-            case 3: sr.polindrom1(); break; //  палиндромность
-            default: System. exit(0); break;
+        Invitetion.invite();
+        Choice.option();
+
+        System.out.printf("%n  will you continue? (Y/N) %n");
+        Scanner ScanOption = new Scanner(System.in); // input by keyboard
+        String MyOption = ScanOption.nextLine();    // take a variable
+        if (MyOption.matches("[^(n|N)]") == true) main(args); // check - is it continue?
+    }
+}
+/*
+        ContinueSR continSR= new ContinueSR();
+        if (continSR.continueSR()==true) {main(args);} // recursion
+    }
+}
+class  ContinueSR {
+   public boolean continueSR(); {
+    System.out.printf("%n  will you continue? (Y/N) %n") ;
+    Scanner ScanOption = new Scanner(System.in); // input by keyboard
+    String MyOption = ScanOption.nextLine();    // take a variable
+       return MyOption.matches("[^(n|N)]");// check - is it continue?
         }
-        main(args); // рекурсия
+}
+*/
+class Invitetion { // 2.
+    public static void invite() {
+        String inv = "Enter the procedure number: %n 1.Sum of numbers, %n 2.Selection of the smallest, %n 3.Checking the word array for palindromicity, %n Other ==> exit. %n";
+        System.out.printf("%n" + inv);
+    }
+}
+
+class Choice { // 3.
+    public static void option() {
+        Scanner ScanOption = new Scanner(System.in); // input by keyboard
+        Short MyOption = ScanOption.nextShort();    // take a variable
+        switch (MyOption) {
+            case 1:  //  Sum
+                Summ Sum = new Summ();
+                Sum.sum();
+                break;
+            case 2:
+                Min min0 = new Min();
+                System.out.println("Min: " + (String.valueOf(min0.min())));
+                break; // наименьшее
+            case 3:
+                palindrom polin = new palindrom();
+                polin.find();
+                break; //  палиндромность
+            default:
+                System.exit(0);
+                break;
+        }
+    }
+}
+
+class InputMassive {   //  4.
+
+    public String[] TxtMass(String heading) {
+        String txtForEnter = "Enter the initial data separated by a space (\" \") : %n";
+        System.out.printf("%n" + heading + "%n" + txtForEnter);
+        Scanner myVar = new Scanner(System.in); // input by keyboard
+        String line = myVar.nextLine();           // take a variable
+        while (line.indexOf("  ") > 0) line = line.replace("  ", " ");
+        String masT[] = line.split(" ");   // String Massive select by space
+        return (masT);
     }
 
-    public  String [] enterMass(String t) { //  ----- 0 - ВВОД МАССИВА (СТРОКИ ДАННЫХ) ----------------
-        System.out.println();
-        System.out.println(t);
-        System.out.println();
-        System.out.println("Введите исходные данные через пробел (\" \") :");
-        Scanner myVar = new Scanner(System.in); // ввод с клавы
-        String s = myVar.nextLine();              // берем переменную
-        String masT[] = s.split(" ");               // Str массив выделим по пробелу
-        return masT;
+    public double[] DoubleMassive(String txt) {
+        String masT[] = TxtMass(txt);
+        double masDouble[] = new double[masT.length];
+        for (int i = 0; i < masDouble.length; i++) {
+            if (isNumeric(masT[i]) == true) {
+                masDouble[i] = Double.parseDouble(masT[i]);//  Str-->Double massive
+            } else {
+                NomberFormatError(masT[i]); // INPUT ERROR MESSAGE (ENTER NUMBER!)
+            }
+        }
+        return (masDouble);
     }
 
-    public float add() { // ---- 1Я ПРОЦЕДУРА --------------
-        String t="Определяем сумму введенных чисел";
-        String masT[]=enterMass(t);
-        float masF[] = new float[masT.length];                      // пустой Float массив по величине Str массива
-        float sum_=0;
-        for (int i = 0; i < masF.length; i++){
-            {try {masF[i] = Float.parseFloat(masT[i]);} catch(NumberFormatException e) {er(masT[i]);}}
-             sum_=sum_+masF[i];}// перевели Str-->Float, суммируем
-        return sum_;
+    public boolean isNumeric(String strNum) {
+        return strNum.matches("-?\\d+(\\.\\d+)?"); // check - is it a nomber?
     }
 
-    public void read_() { // ------------- 2Я ПРОЦЕДУРА --------------------------------------------------
-        String t = "Определяем наименьшее из введенных чисел";
-        int k=-1; // метка для ошибки ввода
-        String masT[] = enterMass(t);
-        float masF[] = new float[masT.length];                      // пустой Float массив по величине Str массива
-        try {
-        for (int i = 0; i < masF.length; i++)
-        { k=i; masF[i] = Float.parseFloat(masT[i]);}// перевели Str-->Float
-        Arrays.sort(masF);                                                 // сортировка
-        String s = Float.toString(masF[0]);                               // первое- минимальное при сортировке, формат результата с "." или без
-        if (masF[0] == Math.round(masF[0])) {s = Integer.toString((int) masF[0]);} else {s = Float.toString(masF[0]);};
-        prnt("Минимальное число в массиве : ", s," ", "====="); // декоратор
-        }catch(NumberFormatException e) {er(masT[k]);} // при шибке ввода
+    public void NomberFormatError(String txt) {        // INPUT ERROR MESSAGE (ENTER NUMBER!)
+        System.out.println(txt + " - ERROR IN LINE. NON-NUMERICAL VALUE.");
     }
+}
 
-    public void polindrom1() { // ------------- 3Я ПРОЦЕДУРА --------------------------------------------------
-        String t = "Определяем палиндромы в строке";
-        String masT[] = enterMass(t); // ввели массив слов в строку
-        short k1 = 0; short k2 = 0; // метки - при найденом полиндроме =1
-        prnt("Палиндромы в строке ", "","", ""); //         декоратор
-        for (int i = 0; i < masT.length; i++) {  // проверка каждого элемента массива
-            char[] p = masT[i].toCharArray();
-            for (int i0 = 0; i0 < p.length; i0++) {
-                if (p[i0] != p[p.length - i0 - 1]) { // проверка каждой буквы слова эемента при встречном чтении
-                    k2 = 0; // при несовпадении букв - обнул и выход из слова
+class Summ {        // 6.
+    public void sum() {
+        InputMassive inpMass = new InputMassive();
+        String heading = "Determine the sum of the entered numbers";
+        double[] masDouble = inpMass.DoubleMassive(heading);
+        double sum = 0;
+        for (int i = 0; i < masDouble.length; i++) {
+            sum = sum + masDouble[i];
+        }
+        System.out.println("Sum: " + String.valueOf(sum));
+    }
+}
+
+class Min { //  7.
+    public Double min() {
+        InputMassive inpMass = new InputMassive();
+        String heading = "Determine the smallest of the entered numbers";
+        double[] masDouble = inpMass.DoubleMassive(heading);
+        double min0 = masDouble[0]; // first value
+        for (int i = 1; i < masDouble.length; i++) { // find minimum in cicle
+            if (String.valueOf(masDouble[i]).length()>0 & min0 < masDouble[i]) min0 = masDouble[0];
+        }
+        return min0;
+    }
+}
+
+class palindrom {
+    public void find() {
+        InputMassive inpMass = new InputMassive();
+        String heading = "Defining palindromes in a string";
+        String[] masT = inpMass.TxtMass(heading);
+        short lettersMatch = 0;
+        short palyindromeFound = 0;
+        System.out.printf("%n" + "Palindromes in a line :" + "%n");
+        for (int Word = 0; Word < masT.length; Word++) {           // checking each Word of the Words array
+            char[] Letters = masT[Word].toCharArray();
+            for (int letter = 0; letter < Letters.length; letter++) { // checking each letter of the Word array
+                if (Letters[letter] != Letters[Letters.length - letter - 1]) { // checking each letter of a word element when reading from the beginning and from the end, towards
+                    lettersMatch = 0; // if the letters do not match - zeroing and exiting the word
                     break;
                 } else {
-                    k2 = 1;
-                };
-            }
-            if (k2 == 1) {
-                System.out.println(masT[i]); k1 =+ 1; // вывели полиндром, метка - найден
-                k2 = 0;
+                    lettersMatch = 1;
                 }
+                ;
+            }
+            if (lettersMatch == 1) {
+                System.out.print(masT[Word] + " ");
+                palyindromeFound = +1;
+                lettersMatch = 0;
+            }
         }
-        if (k1 == 0) System.out.println("не обнаружены".toUpperCase()); // если таких нет в строке
+        System.out.println();
+        if (palyindromeFound == 0) System.out.print("NOT FOUND"); //
     }
 
-    public void er(String t) // --------- СООБЩЕНИЕ ОШИБКИ ВВОДА (ВВЕЛИ НЕ ЧИСЛО!) -----------------
-    {System.out.println(t+" - ОШИБКА В СТРОКЕ. НЕЧИСЛОВОЕ ЗНАЧЕНИЕ.");}
-
-    public void prnt (String a, String b,String d, String f ){  // ------- ДЕКОРАТОР ----------
-        String c=d.repeat(a.length())+f;
-        System.out.println(c);
-        System.out.println(a.toUpperCase()+b);
-        System.out.println(c);
-    }
-    }
+}
